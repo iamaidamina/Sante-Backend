8d62204a9932dda50e75feca0b178fa439b050c2
Luis Miguel Cote <ing.luismiguelcote@gmail.com>
2026-03-02 00:13:17 -0500

    Update appointments.routes.js

--- src/routes/appointments.routes.js
+++ src/routes/appointments.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middlewares/auth.middleware");
-const pool = require("../config/db"); 
+const pool = require("../db/connection"); 
 
 // CREATE appointment
 /**
