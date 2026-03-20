19ff044a9a1900c56d7fba2e1478bb3e1e7deac7
Aida  Mina <iamaidamina@gmai.com>
2026-03-20 03:11:59 -0500

    modificando tabla entregas en el esquema

--- src/routes/catalog.routes.js
+++ src/routes/catalog.routes.js
@@ -29,34 +29,41 @@ router.get('/especialidades', verifyToken, async (req, res) => {
 
 
 router.post("/create-table", async (req, res) => {
-    try {
-        const createTableQuery = `
-            CREATE TABLE IF NOT EXISTS domiciliarios (  -- ← IF NOT EXISTS evita error
-                id_domiciliario INT AUTO_INCREMENT PRIMARY KEY,
-                nombre_domiciliario VARCHAR(250),
-                direccion_domiciliario VARCHAR(150),
-                tipo_documento VARCHAR(150),
-                numero_documento VARCHAR(250),
-                documento_identidad VARCHAR(250),
-                estado ENUM('activo','inactivo') DEFAULT 'activo',  -- ← COMA AQUÍ
-                fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )
+  try {
+    const createTableQuery = `
+            CREATE TABLE entregas (
+    id_entrega INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    lugar_compra VARCHAR(150),
+    id_domiciliario INT,
+    fecha_llegada DATE,
+    nombre_producto VARCHAR(250),
+    orden_medica VARCHAR(250),
+    comentario VARCHAR(250),
+    estado ENUM('aceptado','pendiente','entregado'),
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
+        ON DELETE CASCADE,
+        FOREIGN KEY (id_domiciliario) REFERENCES domiciliarios(id_domiciliario)
+        ON DELETE SET NULL
+)
         `;
 
-        const [result] = await pool.query(createTableQuery);
+    const [result] = await pool.query(createTableQuery);
 
-        res.status(201).json({
-            message: "Table 'domiciliarios' created successfully",  // ← Corregido
-            table_name: 'domiciliarios'                             // ← Corregido
-        });
+    res.status(201).json({
+      message: "Table 'domiciliarios' created successfully",  // ← Corregido
+      table_name: 'domiciliarios'                             // ← Corregido
+    });
 
-    } catch (error) {
-        console.error("Error creating table:", error);
-        res.status(500).json({ 
-            message: "Internal server error", 
-            error: error.message  // ← Para debug
-        });
-    }
+  } catch (error) {
+    console.error("Error creating table:", error);
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message  // ← Para debug
+    });
+  }
 });
 
 
