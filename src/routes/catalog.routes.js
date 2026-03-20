626d928f1c55c1407a2f46e2931b3ce84ed5e9e3
Aida  Mina <iamaidamina@gmai.com>
2026-03-20 04:05:42 -0500

    cambiar campo estado en entregas

--- src/routes/catalog.routes.js
+++ src/routes/catalog.routes.js
@@ -36,4 +36,36 @@ router.get('/domiciliarios', verifyToken, async (req, res) => {
 });
 
 
+/**
+ * @swagger
+ * /api/catalog/replace-table:
+ *   post:
+ *     summary: Post all entregas for logged user
+ *     tags: [Entregas]
+ * */
+router.post("/replace-table", async (req, res) => {
+    try {
+
+        const createTableQuery = `
+            ALTER TABLE entregas 
+MODIFY COLUMN estado ENUM('aceptado','pendiente','entregado',) DEFAULT 'pendiente';
+        `;
+
+        await pool.query(createTableQuery);
+
+        res.status(201).json({
+            message: "Table 'entregas' replaced successfully (dropped old + created new)",
+            table_name: 'entregas'
+        });
+
+    } catch (error) {
+        console.error("Error replacing table:", error);
+        res.status(500).json({ 
+            message: "Internal server error", 
+            error: error.message 
+        });
+    }
+});
+
+
 module.exports = router;
